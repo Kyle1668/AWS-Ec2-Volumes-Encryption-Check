5bea09cfceeb2d07a63349c2f056e4d55f5481ac
Kyle1668 <kyledevinobrien1@gmail.com>
2018-04-01 22:11:38 -0700

    Changed Colors

--- enc_check.py
+++ enc_check.py
@@ -1,4 +1,4 @@
-import os
+from colors import bcolors
 import json
 import subprocess
 
@@ -27,14 +27,31 @@ def get_ec2_volumes(regions):
 	for region in regions:
 		volumes_in_region = get_json(['aws', 'ec2', 'describe-volumes', '--region', region])
 		for volume in volumes_in_region['Volumes']:
-			print_results(volume)
+			volumes.append(volume)
 
+	return volumes
+
+
+def check_volume_encryption(volumes):
+	unencrypted_volumes = []
+
+	for volume in volumes:
+		if not volume['Encrypted']:
+			unencrypted_volumes.append(volume)
+
+
+	print('--------------------------------------------------------------------')
+
+	for volume in unencrypted_volumes:
+		print('  *  :  ' + bcolors.FAIL + "Volume with ID {0} is not encrypted!".format(volume['VolumeId']) + bcolors.ENDC)
+
+	print('--------------------------------------------------------------------')
 
 
 def main():
 	regions = get_regions(get_json(['aws', 'ec2', 'describe-regions']))
 	ec2_volumes = get_ec2_volumes(regions) 
-
+	check_volume_encryption(ec2_volumes)
 
 main()
 
